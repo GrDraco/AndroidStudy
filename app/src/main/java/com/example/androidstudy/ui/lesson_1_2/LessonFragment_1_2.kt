f662f834ea0bd5cbc8393c70fd5142e0332ceb65
GrDraco <IgrikWork@gmail.com>
2021-03-18 20:00:42 +0500

    Created two empty activity

--- app/src/main/java/com/example/androidstudy/ui/lesson_1_2/LessonFragment_1_2.kt
+++ app/src/main/java/com/example/androidstudy/ui/lesson_1_2/LessonFragment_1_2.kt
@@ -1,4 +1,4 @@
-package com.example.androidstudy.ui.gallery
+package com.example.androidstudy.ui.lesson_1_2
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,9 +10,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidstudy.R
 
-class GalleryFragment : Fragment() {
+class LessonFragment_1_2 : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var galleryViewModel: LessonViewModel_1_2
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -20,7 +20,7 @@ class GalleryFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         galleryViewModel =
-                ViewModelProvider(this).get(GalleryViewModel::class.java)
+                ViewModelProvider(this).get(LessonViewModel_1_2::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
--- app/src/main/java/com/example/androidstudy/ui/lesson_1_2/LessonViewModel_1_2.kt
+++ app/src/main/java/com/example/androidstudy/ui/lesson_1_2/LessonViewModel_1_2.kt
@@ -1,10 +1,10 @@
-package com.example.androidstudy.ui.gallery
+package com.example.androidstudy.ui.lesson_1_2
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class LessonViewModel_1_2 : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
